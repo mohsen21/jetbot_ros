6ca4bc74453c33d6d4b4a5e2b2d4b1cf22548253
mohsen <mohsenshiri21@gmail.com>
2024-09-22 19:04:24 +0330

    update for humble

--- launch/jetbot_waveshare.launch.py
+++ launch/jetbot_waveshare.launch.py
@@ -4,22 +4,27 @@
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     
-   
-    
-    motor_controller = Node(package='jetbot_ros', node_executable='motors_waveshare',
-                            output='screen', emulate_tty=True)  
-    
-    rtp_output = DeclareLaunchArgument('rtp_output', default_value="DUSTINF-LT1.fios-router.home:1234")
-    
+    # Declare launch arguments
+    rtp_output = DeclareLaunchArgument(
+        'rtp_output', 
+        default_value="DUSTINF-LT1.fios-router.home:1234"
+    )
 
-                        
+    # Define the motor controller node
+    motor_controller = Node(
+        package='jetbot_ros',
+        executable='motors_waveshare',  # Updated to 'executable' for ROS Humble
+        output='screen', 
+        emulate_tty=True
+    )  
+    
+    # Return the launch description
     return LaunchDescription([
-        motor_controller,
         rtp_output,
+        motor_controller,
     ])
